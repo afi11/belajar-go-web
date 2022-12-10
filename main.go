4769950f679600b99b1cdd5fc6bdd372b4454031
afif.fatakhul <fatakhulafi11@gmail.com>
2022-12-10 20:54:15 +0700

    Render File HTML

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	// Tambahkan ini untuk latihan ke 3
+	"html/template"
+	"path"
 )
 
-func handlerIndex(w http.ResponseWriter, r *http.Request) {
-	var message = "Welcome"
-	w.Write([]byte(message))
-}
+// func handlerIndex(w http.ResponseWriter, r *http.Request) {
+// 	var message = "Welcome"
+// 	w.Write([]byte(message))
+// }
 
 func handlerHello(w http.ResponseWriter, r *http.Request) {
 	var message = "Hello world!"
@@ -16,13 +20,32 @@ func handlerHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handlerIndex)
-	http.HandleFunc("/index", handlerIndex)
+	// http.HandleFunc("/", handlerIndex)
+	// http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/hello", handlerHello)
 
 	// 2. Routing Static Assets
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
 
+	// 3. Render HTML File
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var filepath = path.Join("views", "index.html")
+		var tmpl, err = template.ParseFiles(filepath)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var data = map[string]interface{}{
+			"title": "Learning Golang Web",
+			"name":  "Batman",
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 	var address = "localhost:9000"
 	fmt.Printf("server started at %s\n", address)
 	err := http.ListenAndServe(address, nil)
