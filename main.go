dc71f4dc553582a175b7e14a9be32023808f4a23
afif.fatakhul <fatakhulafi11@gmail.com>
2022-12-10 21:33:05 +0700

    Render Partial HTML

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	// Tambahkan ini untuk latihan ke 3
 	"html/template"
-	"path"
 )
 
 // func handlerIndex(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +18,9 @@ func handlerHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(message))
 }
 
+// Tambah untuk step 4
+type M map[string]interface{}
+
 func main() {
 	// http.HandleFunc("/", handlerIndex)
 	// http.HandleFunc("/index", handlerIndex)
@@ -28,24 +30,53 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
 
 	// 3. Render HTML File
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	var filepath = path.Join("views", "index.html")
+	// 	var tmpl, err = template.ParseFiles(filepath)
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	var data = map[string]interface{}{
+	// 		"title": "Learning Golang Web",
+	// 		"name":  "Batman",
+	// 	}
+
+	// 	err = tmpl.Execute(w, data)
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	}
+	// })
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var filepath = path.Join("views", "index.html")
-		var tmpl, err = template.ParseFiles(filepath)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		var data = M{"name": "Batman"}
+		var tmpl = template.Must(template.ParseFiles(
+			"views/index.html",
+			"views/_header.html",
+			"views/_message.html",
+		))
 
-		var data = map[string]interface{}{
-			"title": "Learning Golang Web",
-			"name":  "Batman",
-		}
-
-		err = tmpl.Execute(w, data)
+		var err = tmpl.ExecuteTemplate(w, "index", data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Batman"}
+		var tmpl = template.Must(template.ParseFiles(
+			"views/about.html",
+			"views/_header.html",
+			"views/_message.html",
+		))
+
+		var err = tmpl.ExecuteTemplate(w, "about", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	var address = "localhost:9000"
 	fmt.Printf("server started at %s\n", address)
 	err := http.ListenAndServe(address, nil)
